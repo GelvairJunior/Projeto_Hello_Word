4e2c7fe3e2b762457ccfc249a2a95ededafecf0a
GelvairJunior <gelvair.schumann.jr@gmail.com>
2026-03-26 12:04:47 -0300

    Avançando em estrutura de dados de ordenação, ordenação por insercao sofisticado, e ordenacao por arvore 'heap' (arrumar nomes)

--- Java_Basic/Aulas_e_Testes_Java/src/edu/Algoritmos/livro/ordenacao/OrdenacaoPrincipal.java
+++ Java_Basic/Aulas_e_Testes_Java/src/edu/Algoritmos/livro/ordenacao/OrdenacaoPrincipal.java
@@ -4,9 +4,9 @@ public class OrdenacaoPrincipal {
 	public static void main(String[] args) {
 		Ordenacao_De_Vetores_Direta ordV = new Ordenacao_De_Vetores_Direta();
 		Item[] tabela = new Item[23];
-		int[] des = {0,8,40,45,7,6,9,12,15,16,34,32,38,14,24,26,28,27,92,1,20,17,22, 21};
-		String[] sas = {"A","L","G","O","R","I","T","M","O","_","E","_","E","S","T","R","U","T","U","R","A", "_", "D", "E", "_"};
-		
+		int[] des = { 0,8,40,45,7,6,9,12,15,16,34,32,38,14,24,26,28,27,92,1,20,17,22, 21};
+		String[] sas = { "A","L","G","O","R","I","T","M","O","_","E","_","E","S","T","R","U","T","U","R","A", "_", "D", "E", "_"};
+		//9, 5, 3, 4,"$", "$", "$", "$",
 		
 		
 		for(int i = 0; i < tabela.length; i++) {
@@ -19,7 +19,9 @@ public class OrdenacaoPrincipal {
 		//tabela = ordV.ordSelecao(tabela);
 		//tabela = ordV.ordBubbleSort(tabela);
 		//tabela = ordV.ordShakerSort(tabela);
-		
+		//tabela = ordV.ordInsecaoSofisticado(tabela);
+		tabela = ordV.ordHeap(tabela);
+		System.out.println("-------------------------");
 		
 		for(int i = 0; i < tabela.length; i++) {
 			System.out.println(tabela[i]);
--- Java_Basic/Aulas_e_Testes_Java/src/edu/Algoritmos/livro/ordenacao/Ordenacao_De_Vetores_Direta.java
+++ Java_Basic/Aulas_e_Testes_Java/src/edu/Algoritmos/livro/ordenacao/Ordenacao_De_Vetores_Direta.java
@@ -2,19 +2,13 @@ package edu.Algoritmos.livro.ordenacao;
 
 public class Ordenacao_De_Vetores_Direta {
 	Item[] ordInsercao(Item[] a){
-		for(int i = 0; i<a.length; i++) {
-			System.out.print(a[i].key + a[i].nome + " ");
-		}
-		System.out.println("");
-		
-		
 		int index;
 		int i, j;
 		Item x;
 		int n = a.length;
 		
 		
-		for(i = 2; i < a.length; i++){
+		for(i = 1; i < n; i++){
 
 	        x = a[i]; // chave inicial
 	        j = i;
@@ -27,14 +21,6 @@ public class Ordenacao_De_Vetores_Direta {
 	        a[j] = x;
 	    }
 		
-		
-		
-		System.out.println("-------------------------");
-		for(i = 0; i<a.length; i++) {
-			System.out.print(a[i].key + a[i].nome + " ");
-		}
-		System.out.println("");
-		
 		return a;
 	}
 	
@@ -43,12 +29,8 @@ public class Ordenacao_De_Vetores_Direta {
 		Item x;
 		int n = a.length;
 		
-		for(i = 0; i<n; i++) {
-			System.out.print(a[i].key + a[i].nome + " ");
-		}
-		
-		for(i = 2; i <n; i++) {
-			x = a[i]; j = i; L = 1; R = i;
+		for(i = 1; i <n; i++) {
+			x = a[i]; j = i; L = 0; R = i;
 			
 			while (L<R) {
 				m = (L+R)/2;
@@ -57,18 +39,12 @@ public class Ordenacao_De_Vetores_Direta {
 				}else {R = m;}
 			}
 			
-			for(j = i; j < R; j++) {
+			for(j = i; j > R; j--) {
 				a[j] = a[j-1];
 			}
 			
 			a[R] = x;
 			
-		}
-		
-		System.out.println("");
-		System.out.println("-------------------------");
-		for(i = 0; i<n; i++) {
-			System.out.print(a[i].key + a[i].nome + " ");
 		}
 		
 		return a;
@@ -77,11 +53,6 @@ public class Ordenacao_De_Vetores_Direta {
 	Item[] ordSelecao(Item[] a) {
 		int j, k;
 		Item x;
-		
-		for(int i = 0; i<a.length; i++) {
-			System.out.print(a[i].key + a[i].nome + " ");
-		}
-		System.out.println("");
 		
 		for(int i = 0; i < a.length; i++) {
 			k = i; x = a[i];
@@ -93,27 +64,113 @@ public class Ordenacao_De_Vetores_Direta {
 			a[k] = a[i]; a[i] = x;
 		}
 		
-		for(int i = 0; i<a.length; i++) {
-			System.out.print(a[i].key + a[i].nome + " ");
-		}
-		System.out.println("");
-		
 		return a;
 	}
 	
 	Item[] ordBubbleSort(Item[] a) {
-		int i, j, k;
+		int i, j;
 		Item x;
 		
+		for(i = 0; i < a.length; i++) {
+			for(j = a.length-1; j > i; j--) {
+				if(a[j-1].key > a[j].key) {
+					x = a[j-1]; a[j-1] = a[j]; a[j] = x;
+				}
+			}
+		}
 		
 		return a;
 	}
 	
 	Item[] ordShakerSort(Item[] a) {
-		int i, j, k;
+		int j, k;
 		Item x;
+		int L, R;
+		R = a.length -1; k = a.length; L = 0;
+		
+		do{
+			for(j = R; j > L; j--) {
+				if(a[j-1].key > a[j].key) {
+					x = a[j-1]; a[j-1] = a[j]; a[j] = x; k = j;
+				
+				}
+			}
+			L=k;
+			
+			for(j = L; j < R; j++) {
+				if(a[j].key > a[j+1].key) {
+					x = a[j]; a[j] = a[j+1]; a[j+1] = x; k = j;
+				}
+			}
+			R=k;
+		}while(L < R);
 		
 		
 		return a;
+	}
+	
+	Item[] ordInsecaoSofisticado(Item[] a) {
+		int i, j, s, k;
+		int m;
+		Item x;
+		int t = 4;
+		int[] h = new int[t];
+
+		h[0] = 9; h[1] = 5; h[2] = 3; h[3] = 1;
+		
+		for(m = 0; m < t; m++) {
+			
+			k = h[m];// s = -k; sentinela
+			
+			for(i = m; i< a.length; i++) {
+				
+				x = a[i]; j = i;
+				
+				while(j >=m && a[j-m].key > x.key) {
+					a[j] = a[j-m]; 
+					j = j-m ;
+				}
+				
+				a[j] = x;
+			}
+		}
+		
+		return a;
+	}
+	
+	Item[] ordHeap(Item[] a) {
+		//versão mlr da seleção
+		Item x;
+		int L, R;
+		int n = a.length -1;
+		
+		L = (n/2);
+		R = n;
+		while (L > 0) {
+			L--; 
+			escorrega_Sift(a, L, R);
+		}
+		while(R>0) {
+			x = a[0]; a[0] = a[R]; a[R] = x;
+			R--;
+			this.escorrega_Sift(a, L, R);
+		}
+		
+		return a;
+	}
+	
+	private void escorrega_Sift(Item[] a, int L, int R) {
+		int i, j;
+		Item x;
+		
+		i = L; j = 2*L; x = a[L];
+		if(j < R && a[j+1].key > a[j].key) {j++;}
+		while(j <= R && x.key < a[j].key) {
+			System.out.println("R = "+ R + " j = " + j + " L = " + L + " i = "+ i);
+			a[i] = a[j]; i = j; j = 2*j;
+			if(j < R && a[j].key < a[j+1].key) {j++;}
+		}
+		a[i] = x;
+		
 	}
 }
