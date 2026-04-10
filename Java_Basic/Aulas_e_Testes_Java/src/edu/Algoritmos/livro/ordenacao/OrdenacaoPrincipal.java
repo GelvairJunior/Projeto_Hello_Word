4ca3283588cdda67c6b063bde0773e04634300d3
GelvairJunior <gelvair.schumann.jr@gmail.com>
2026-04-10 17:59:12 -0300

    nao conseguir fazer funcionar o algoritmo deve-se voltar mais tarde, segue o livro para algoritmos posteriores

--- Java_Basic/Aulas_e_Testes_Java/src/edu/Algoritmos/livro/ordenacao/OrdenacaoPrincipal.java
+++ Java_Basic/Aulas_e_Testes_Java/src/edu/Algoritmos/livro/ordenacao/OrdenacaoPrincipal.java
@@ -3,6 +3,7 @@ package edu.Algoritmos.livro.ordenacao;
 public class OrdenacaoPrincipal {
 	public static void main(String[] args) {
 		Ordenacao_De_Vetores_Direta ordV = new Ordenacao_De_Vetores_Direta();
+		Ordenacao_Sequencial ordS = new Ordenacao_Sequencial();
 		Item[] tabela = new Item[23];
 		int[] des = { 0,8,40,45,7,6,9,12,15,16,34,32,38,14,24,26,28,27,92,1,20,17,22, 21};
 		String[] sas = { "A","L","G","O","R","I","T","M","O","_","E","_","E","S","T","R","U","T","U","R","A", "_", "D", "E", "_"};
@@ -23,12 +24,15 @@ public class OrdenacaoPrincipal {
 		//tabela = ordV.ordHeap(tabela);
 		//tabela = ordV.ordQuick(tabela, 0, tabela.length-1);
 		//tabela = ordV.ordNonRecursiveQuick(tabela);
-		System.out.println(ordV.findMediana(tabela));
+		//System.out.println(ordV.findMediana(tabela));
+		Item[] tabela2;
+		
+		tabela2 = ordS.ordenacaoPorFusaoVetores(tabela);
 		
 		System.out.println("--------------------------------------------");
 		
-		for(int i = 0; i < tabela.length; i++) {
-			System.out.println(tabela[i]);
+		for(int i = 0; i < tabela2.length; i++) {
+			System.out.println(tabela2[i]);
 		}
 	}
 }
--- Java_Basic/Aulas_e_Testes_Java/src/edu/Algoritmos/livro/ordenacao/Ordenacao_Sequencial.java
+++ Java_Basic/Aulas_e_Testes_Java/src/edu/Algoritmos/livro/ordenacao/Ordenacao_Sequencial.java
@@ -2,38 +2,65 @@ package edu.Algoritmos.livro.ordenacao;
 
 public class Ordenacao_Sequencial {
 
-	void ordenacaoPorFusaoVetores(Item[] b){
-		Item[] a = new Item[b.length*2];
-		for (int j=0, i = 0; i < a.length-1; i++) {
-			if(i < b.length) {
-				a[i] = b[i];
-			}else {
-				a[i] = b[j];
-				j++;
-			}
+	Item[] ordenacaoPorFusaoVetores(Item[] b){
+		int n = b.length;
+		Item[] a = new Item[n * 2];
+
+		for (int i = 0; i < n; i++) {
+		    a[i] = b[i];
+		    a[i + n] = b[i];
 		}
 		
 		int i, j, k, l, t, h, m, p, q, r;
 		boolean up;
-		int n = b.length -1;
+		
 		
 		up = true; p = 1;
 		
-		do { 
-			h= 1; m = n;
+		
+		do { h = 1; m = n;
 			if(up) {
-				i = 1; j = n; k = n+1; l = n*2;
+				i = 0; j = n - 1; k = n; l = n*2 - 1;
 			}else {
-				k = 1; l = n; i = n+1; j = n*2;
+				k = 0; l = n - 1; i = n; j = n*2 - 1;
 			}
 			
 			do {
+				if(m >= p) { q = p;} else {q = m;}
+				m = m-q;
+				
+				if(m >= p) {r = p; }else {r = m;}
+				
+				while(q != 0 && r != 0) {
+					System.out.println(a[i] + " < Confere < " + a[j]);
+					if (a[i].key < a[j].key) {
+						System.out.println(a[k] + " Trocando " + a[i]);
+						a[k] = a[i]; k = k+h; i++; q--;
+					}else {
+						System.out.println(a[k] + " Trocando " + a[j]);
+						a[k] = a[j]; k = k+h; j--; r--;
+					}
+				}
+				
+				while (r > 0) {
+					a[k] = a[j]; k = k+h; j--; r--;
+				}
+				
+				while (q > 0) {
+					a[k] = a[i]; k = k+h; i++; q--;
+				}
 				
 				h = -h; t = k; k = l; l = t;
-			}while(m == 0);
+
+			}while(m > 0);
 			
 			up = !up; p = p*2;
-		}while(p>=n);
-		
+		}while(p <= n);
+		if(!up) {
+			for(i = 0; i < n; i++) {
+				a[i] = a[i+n];
+			}
+		}
+		return a;
 	}
 }
